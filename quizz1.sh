8a58684c7e6756c5f6004c564ad888e7dd87d911
harriet-web <73937423+harriet-web@users.noreply.github.com>
2023-07-19 08:37:37 -0400

    Add files via upload

--- quizz1.sh
+++ quizz1.sh
@@ -1,48 +1,46 @@
 #!/bin/bash
 #
+#
 
+printf "Welcome To Sports Masters. PLease fill the registration form below to join.\n"
+read -p "Please enter your name: " name
+read -p "Enter your gender(M/F): " gender
+read -p "Enter your email address: " email
+read -p "Enter the type of sport activity you like: " sport
 
-printf "Welcome to our company.........\n"
-read -p  "Enter the domain name you want to use: " name
-printf "Choose any of the tools to use\n"
-printf "1.sublist3r\n2.theHarvester\n" 
-read -p "Enter 1 or 2 to select the tool you want to use: " tool
-toolname=""
-sublist3r=`sublist3r -v -d $name`
-theHarvester=`theHarvester -d $name -l '20' -b bing`
-if [ $tool -eq 1 ] 
-then
-	toolname="sublist3r"
-	##printf "The tool name has been saved\n"
-elif [ $tool -eq 2 ] 
-then
-	toolname="theHarvester"
-fi
-while [ $tool -ne 1 ] && [ $tool -ne 2 ];
-do
-	read -p "Enter 1 or 2: " tool
+case $sport in
+	"Football" | "basketball")
+		printf "Thank you for your interest in the most popular sport.\n"
+		;;
+	"Cricket")
+		printf "Not bad. Have fun\n"
+		;;
+	"Horse Racing")
+		printf "You sure are very energetic.\n"
+		;;
+	"Esports")
+		if [ "$gender" = "F" ];then
+			printf "There are few females here.\n"
+			read -p "Do you wish to continue with your selection? Y/N: " answer
+			if [ "$answer" = "Y" ];then
+			       printf "Welcome to the new world.\n"
+		       else
+		              printf "Sorry about that, see you some other time."
+	                fi
 
-done
+                        fi
+                           printf "Welcome to Espots activity $name. Let's have fun....\n"
+                                ;;
+                      *)
+                          printf "Sorry, we don't offer your type of sport activity."
+                               ;;
+esac		
 
-printf "You've entered a valid tool name\n"
-
-if [ $tool = 1 ]
-then
-	printf "$name"
-        "$sublist3r"
+printf "You registered with the following details:\n\n"
 sleep 2
-read -p "Enter a name to save your file: " respons
-##"$sublist3r" | grep -i "*google*" >> $respons
-printf "Saving result in the file $respons\n"
-printf "Result can be found in $PWD/$respons\n"
-fi
+printf "Name: $name\n"
+printf "Gender: $gender\n"
+printf "Email: $email\n"
+printf "Type of sport: $sport\n"
 sleep 2
- if [ $tool = 2 ]
- then
-	 printf "$name"
-	 "$theHarvester"
- 
-read -p "Enter the file name to save your file: " input
-printf "Saving result in the file $input\n"
-printf "Results can be found in $PWD/$input"
- fi
+printf "Thanks for joining Sports Masters. Let's have fun in the world of sports"
